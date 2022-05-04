d6a8197781bb85c15cbc41602af2e68f823e1d14
Dariusz Ryszka <dariusz.ryszka@onet.pl>
2022-05-04 17:59:41 +0200

    Program for the job

--- Zad_1/Program.cs
+++ Zad_1/Program.cs
@@ -2,11 +2,124 @@
 
 namespace Zad_1
 {
+    public class Samochod
+    {
+        private string marka;
+        private string model;
+        private int iloscDrzwi;
+        private int pojemnoscSilnika;
+        private double srednieSpalanie;
+        private static int iloscSamochodow = 0;
+
+
+
+        public Samochod()
+        {
+            marka = "nieznana";
+            model = "nieznany";
+            iloscDrzwi = 0;
+            pojemnoscSilnika = 0;
+            srednieSpalanie = 0;
+            iloscSamochodow = iloscSamochodow + 1;
+        }
+
+        public Samochod(string _marka, string _model, int _iloscDrzwi, int _pojemnoscSilnika, double _srednieSpalanie)
+        {
+            marka = _marka;
+            model = _model;
+            iloscDrzwi = _iloscDrzwi;
+            pojemnoscSilnika = _pojemnoscSilnika;
+            srednieSpalanie = _srednieSpalanie;
+            iloscSamochodow += 1;
+        }
+
+        private double ObliczSpalanie(double dlugoscTrasy)
+        {
+            double spalanie = (srednieSpalanie * dlugoscTrasy) / 100;
+            return spalanie;
+        }
+
+        public double ObliczKosztPrzejazdu(double dlugoscTrasy, double cenaPaliwa)
+        {
+            double kosztPrzejazdu = ObliczSpalanie(dlugoscTrasy) * cenaPaliwa;
+            return kosztPrzejazdu;
+        }
+
+        public void WpiszInfo()
+        {
+            Console.WriteLine("Marka: " + marka);
+            Console.WriteLine("Model: " + model);
+            Console.WriteLine("Ilosc Drzwi: " + iloscDrzwi);
+            Console.WriteLine("pojemnosc silnika: " + pojemnoscSilnika);
+            Console.WriteLine("Srednie spalanie: " + srednieSpalanie);
+        }
+
+        public static void WypiszIloscSamochodow()
+        {
+            Console.WriteLine("Ilosc samochodów " + iloscSamochodow);
+        }
+
+        public string Marka
+        {
+            get { return marka; }
+            set { marka = value; }
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public int IloscDrzwi
+        {
+            get { return iloscDrzwi; }
+            set { iloscDrzwi = value; }
+        }
+
+        public int PojemnoscSilnika
+        {
+            get { return pojemnoscSilnika; }
+            set { pojemnoscSilnika = value; }
+        }
+
+        public double SrednieSpalanie
+        {
+            get { return srednieSpalanie; }
+            set { srednieSpalanie = value; }
+        }
+
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Samochod s1 = new Samochod();
+            s1.WpiszInfo();
+
+            s1.Marka = "Porsche";
+            s1.Model = "911";
+            s1.IloscDrzwi = 2;
+            s1.PojemnoscSilnika = 3996;
+            s1.SrednieSpalanie = 20;
+
+            s1.WpiszInfo();
+
+            Samochod s2 = new Samochod("Fiat", "126p", 2, 650, 6.0);
+
+            s2.WpiszInfo();
+
+            double kosztPrzejazdu = s2.ObliczKosztPrzejazdu(30.5, 4.85);
+
+            Console.WriteLine("Koszt przejazdu: " + kosztPrzejazdu);
+
+            Samochod.WypiszIloscSamochodow();
+
+            Console.ReadKey();
+
+
         }
     }
 }
