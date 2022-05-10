128246582f2c3b1870d6851ef3f1f761d4d9580e
Dariusz Ryszka <dariusz.ryszka@onet.pl>
2022-05-10 11:45:25 +0200

    Szukanie błedu i zdalny reset pilków

---
+++ Snake/Piece.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    class Piece : Label //dziedziczenie
+    {
+        public Piece(int x, int y) //konstruktor, stworzenie węża o kolorze zielonym 
+        {
+            Location = new Point(x, y);
+            Size = new Size(20, 20);
+            BackColor = Color.Green;
+            Enabled = false;
+        }
+    }
+}
